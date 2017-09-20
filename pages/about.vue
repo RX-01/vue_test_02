46a9798a2f15a3580c59fcd94021099bd903178a
RX-01 <shanobi89@gmail.com>
2017-09-20 19:46:00 -0400

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>About page</h1>
+    <p>Check the source code to see the custom meta tags added with our custom component <code>twitter-head-card</code></p>
+    <p><nuxt-link to="/">Home page</nuxt-link></p>
+  </div> 
+</template>
+
+<script>
+
+export default {
+  head: {
+    title: 'About Page',
+    meta: [
+      { hid: 'description', name: 'description', content: 'About page description' }
+    ]
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 150px;
+  font-size: 20px;
+}
+</style>
