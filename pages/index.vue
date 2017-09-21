83a95cfb8db6c0cb9a77652fa8004a6567cd05be
RX-01 <shanobi89@gmail.com>
2017-09-20 20:36:17 -0400

    api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="container">
-    <h1>Home page ð</h1>
-    <nuxt-link to="/about">About page</nuxt-link>
+    <h2>Users</h2>
+    <ul class="users">
+      <li v-for="user in users">
+        <nuxt-link :to="'/users/'+user.id">{{ user.name }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  head: {
-    title: 'Home page',
-    meta: [
-      { hid: 'description', name: 'description', content: 'Home page description' }
-    ]
+  async asyncData () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+    return { users: data }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
   text-align: center;
-  margin-top: 150px;
-  font-size: 20px;
+  margin-top: 100px;
+  font-family: sans-serif;
+}
+.users {
+  list-style-type: none;
+}
+.users li a {
+  display: inline-block;
+  width: 200px;
+  border: 1px #ddd solid;
+  padding: 10px;
+  text-align: left;
+  color: #222;
+  text-decoration: none;
+}
+.users li a:hover {
+  color: orange;
 }
 </style>
+
+
